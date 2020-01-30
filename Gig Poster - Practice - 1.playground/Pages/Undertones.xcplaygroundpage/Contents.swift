04142ae8830d98fab7a0db98a6dc4d58c261c89e
Russell Gordon <github@russellgordon.ca>
2020-01-30 09:11:18 -0500

    Initial Commit

---
+++ Gig Poster - Practice - 1.playground/Pages/Undertones.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: # Gig Poster - Practice Task 1
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+/*:
+ Each image is exactly 400 pixels wide by 600 pixels tall.
+ 
+ Your goal is to precisely reproduce this image:
+ 
+ ![undertones-no-grid](undertones-no-grid.png "Undertones")
+ ![undertones-with-grid](undertones-with-grid.png "Undertones")
+
+ By precisely reproducing this image, you will demonstrate your understanding of:
+ 
+ * sequence (order of statements in a program)
+ * conditionals (making decisions with ``if`` statements)
+ * iteration (use of loops to repeat statements)
+ 
+ You may use the color constants created below to obtain the correct colors.
+ 
+ Remember that you can refer to the [Canvas class documentation](https://www.russellgordon.ca/canvasgraphics/documentation/classes/canvas), as needed.
+ */
+
+// Create a new canvas
+let canvas = Canvas(width: 400, height: 600)
+
+// COLORS
+let lightGrey = Color(hue: 84, saturation: 6, brightness: 88, alpha: 100)
+let deepRed = Color(hue: 7, saturation: 97, brightness: 72, alpha: 100)
+
+// Begin your solution here...
+
+
+/*:
+ ## Use Source Control
+ 
+ Remember to commit and push your work before the end of evening study on Tuesday, January 21, 2020, please.
+
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas
+
